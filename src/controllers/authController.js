faf0456831a017e6c6c8ce674f0f1e79efb7e078
minnukota381 <minnukota381@gmail.com>
2024-06-23 19:34:49 +0530

    feat: :sparkles: feat: add logout functionality in authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -60,3 +60,12 @@ exports.postLogin = async (req, res) => {
         res.status(500).send('Error logging in');
     }
 };
+
+exports.logout = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log('Error destroying session:', err);
+        }
+        res.redirect('/');
+    });
+};
